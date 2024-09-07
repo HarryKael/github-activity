6039e8b1b7774dd50d01522cf7491194d0a2fc67
HarryKael <harrykael1@gmail.com>
2024-09-07 10:17:51 -0400

    feat: Showing the events, the repo's names and the amount of the same events has occurred.

--- github-activity.py
+++ github-activity.py
@@ -13,8 +13,26 @@ def main():
         # ! Formating the result
         listed = get_data_as_list(result)
         # ! Printing the results
+        last_event = None
+        last_repo = None
+        amount = 0
+        # ! Going through the list
         for l in listed:
-            print(f'{l['type']} to {l['repo']['name']}')
+            # ! Getting the event and the repo
+            event_type = l['type']
+            repo = l['repo']['name']
+            # ? Knowing if the event_type has changed or if the repo_name has changed too
+            if last_event == None or (last_event == event_type and (last_repo == None or last_repo == repo)):
+                amount = amount + 1
+            else:
+                # ! Printing the event and the amount of it
+                print(f'{last_event} - {amount} to {last_repo}')
+                amount = 1
+            # ! Saving the last events and repo's names
+            last_event = event_type
+            last_repo = repo
+        # ! Printing the last one
+        print(f'{last_event} - {amount} to {last_repo}')
 
 if __name__ == '__main__':
     main()
